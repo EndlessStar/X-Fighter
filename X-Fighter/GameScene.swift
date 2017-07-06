0c1152e71eed6224e526f69f1c15474fed5b86b2
user <user@long.local>
2017-07-06 20:10:37 +0800

    添加陨石贴图

--- X-Fighter/GameScene.swift
+++ X-Fighter/GameScene.swift
@@ -37,17 +37,46 @@ class GameScene: SKScene {
         let remove = SKAction.removeFromParent()
         bulletSprite.run(SKAction.sequence([moveAction, remove]))
         
-        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(fire), userInfo: nil, repeats: true)
+//        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(fire), userInfo: nil, repeats: true)
+        
+        let makeBullets = SKAction.sequence([SKAction.perform(#selector(fire), onTarget: self),
+                                           SKAction.wait(forDuration: 0.30, withRange: 0.15)])
+        let makeRocks = SKAction.sequence([SKAction.perform(#selector(addRock), onTarget: self),
+                                           SKAction.wait(forDuration: 0.10, withRange: 0.15)])
+        
+        self.run(SKAction.repeatForever(makeBullets))
+        self.run(SKAction.repeatForever(makeRocks))
     }
     
     func fire() {
         if let sprite = self.childNode(withName: "SpaceShip") {
             let bulletSprite = self.createBulletSprite(at: sprite.position)
+            bulletSprite.physicsBody = SKPhysicsBody(rectangleOf: bulletSprite.size)
+            bulletSprite.physicsBody?.isDynamic = false
             self.addChild(bulletSprite)
             
             let moveAction = SKAction.move(to: CGPoint(x: sprite.position.x, y: self.frame.height), duration: 0.6)
             let remove = SKAction.removeFromParent()
             bulletSprite.run(SKAction.sequence([moveAction, remove]))
+        }
+    }
+    
+    func addRock() {
+        let rock = SKSpriteNode(imageNamed: "meteorite_type_1")
+        rock.position = CGPoint(x: CGFloat(drand48()) * self.frame.width, y: self.frame.height)
+        rock.name = "Rock"
+        rock.xScale = 0.5
+        rock.yScale = 0.5
+        rock.physicsBody = SKPhysicsBody(rectangleOf: rock.size)
+        rock.physicsBody?.usesPreciseCollisionDetection = true
+        self.addChild(rock)
+    }
+    
+    override func didSimulatePhysics() {
+        self .enumerateChildNodes(withName: "Rock") { (node, stop) in
+            if (node.position.y < 0) {
+                node.removeFromParent()
+            }
         }
     }
     
@@ -61,12 +90,22 @@ class GameScene: SKScene {
         return bulletSprite
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-       /* Called when a touch begins */
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//       /* Called when a touch begins */
+//        for touch in touches {
+//            let location = touch.location(in: self)
+//            if let sprite = self.childNode(withName: "SpaceShip") {
+//                let action = SKAction.move(to: location, duration: 0.3)
+//                sprite.run(SKAction.repeatForever(action))
+//            }
+//        }
+//    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             let location = touch.location(in: self)
             if let sprite = self.childNode(withName: "SpaceShip") {
-                let action = SKAction.move(to: location, duration: 0.6)
+                let action = SKAction.move(to: location, duration: 0.2)
                 sprite.run(SKAction.repeatForever(action))
             }
         }
